998e98f208f4c3b01294bfe1492744dd12c93edf
李绎如 <liyiru@liyirudeMacBook-Pro.local>
2026-01-06 23:15:11 -0500

    Add month [id] page

---
+++ app/month/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React, { Suspense } from "react";
+import MonthClient from "./MonthClient";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function MonthPage({ params }: { params: { id: string } }) {
+  return (
+    <Suspense fallback={null}>
+      <MonthClient monthId={params.id} />
+    </Suspense>
+  );
+}
